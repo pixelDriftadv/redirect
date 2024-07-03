8e32cb836164b94842a9e898c7a1528ca9d2ebcd
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-03 19:12:14 +0530

    *

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+import AnimatedGridPattern from '../../components/magicui/animated-grid-pattern';
+import { cn } from '../../lib/utils';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      
+    </main>
+  );
+}
