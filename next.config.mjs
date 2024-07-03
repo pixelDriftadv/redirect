16781da0c30004187387d932249d38cde7948b25
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-03 19:13:14 +0530

    *

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: 'https://pixeldrift.vercel.app/',
+            permanent: true,
+          },
+        ]
+      },
+};
 
 export default nextConfig;
