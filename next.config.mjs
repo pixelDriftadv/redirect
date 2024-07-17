20961a3d9cd09266cf8aa81f3efb550c19f386fa
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-17 21:30:30 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: 'https://youtube.com/playlist?list=PLcp1KqeP2WdnPdDipxOViW6aMkJl3XUQp',
-            permanent: true,
-          },
-        ]
-      },
 };
 
 export default nextConfig;
