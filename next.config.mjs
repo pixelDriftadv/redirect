c1070780a00803730401ef4e700ea54cc8b1ec79
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-22 22:41:41 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '',
+        destination: 'https://www.instagram.com/necroz.xyc/',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
