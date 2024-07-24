b31215e4bf899de5713c8163d0561b573541074e
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-25 01:06:35 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,28 +6,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between py-24 px-6">
             <p className="z-10  whitespace-pre-wrap text-center text-xl font-bold tracking-tighter ">
-          <span className='text-5xl'>
-          yeh emotional chutiya khud toh yeh bkl relation khatam kar nahi paarha toh hum khud hi madad kardete hai kyu hai na kutti? do saal se dekhe jaa rhe h bass bolta h tera jhutha loyalty ke baare mein saale ko ladki log ke baare mein kuch nahi pata isiliye tujhse itna katva liya h<br/><br/>
-            Saale kutte ki aulad kaat rhe ho iska maza aaya na itna din <br/>abb aur nahi kar paogi
-            </span> <br/><br/>
-              saala iss emotional chutiye ka roz kaat te ho na tm? <br/>
-              saala yahi ek no kutta h ladki pe vishwas rakhta h saala katva rha tha itna din se <br/>
-              aur tm bsdk mc aisa ladka ka kat te re ho joh saala kabhi ldki ke aas pass bhi nahi jaata <br/>
-              saale thora toh aukat mein raho khud instagram ka aukat rakhe ho chutiye kahike bolte ho ambani ka website hai, kutte ki aulad khud itna bhi banane ka aukat rakhe ho <br/>
-              jeon_abro tera hi account h mc itz_kalam itz_sahil saale kitne log se pyarr ka natak karte ho khubsurat kutte? joh tera asli aukat ka kaam hai h vahi bol rhe h <br/>
-              aur abe saale jhutha pyar ka natak karte ho kutte saale isske saath <br/>
-              saale ko aaj takk kabhi gaali dete nahi sune h saala humlog khud iske saamne gaali nahi dete itna accha behaviour aur background aaya h banda <br/>
-              saali kutti isko bhi gaali diyo ho <br/>
-              jhutha natak karte ho pyaar ka yeh toh khud hi tereko dusra ladka log se baat karte dekha hua h kitta kahinka tum jaiso pe vishwas rakhta h <br/>
-              rukk saale hum khud iske parents ko bata rhe h yeh kya kya karta h itna din tujh jaise kutti se pyar karta tha <br/>
-              abb koi natak karne ka zarurat nahi h saala abb kabhi tera account ke aas pass bhi nahi dikhega <br/>
-              jaa be saale rakhle free followers bsdk <br/>
-              mera aukat puchogi, software engineer Bengaluru, tera jaise ke aukat k bahar<br/>
-              jaa na saale 10 log k saath pyaar karo 20 log se saath karo abb iska shakal bhi kabhi yaat matt rkhna bhen c*** 100 shaadi krlo pervert saale, paise leke roj naya husband banalo<br/>
-              mc bass iss baar aao bolte huye miscellaneous website kyu bheja tha? <u>randi bkl kahike hum khud bol denge tereko yaha aane bola hi kaun tha, ambani ka website toh hai nahi joh baar baar dekhoge</u> <br/>
-              emails screenshot hai read status ke saath tere parents ko bhi bhej payenge paar yeh londa humein de nahi rha screenshot itna katvane ke baad nahi toh abhi dhud ka dhud ho jaata ki yeh tere jaisi kutti ko phele disturb kiya ya tu iska website mein phele gussi<br/>
-         saale iss baar isko call karne ka koshish karo pakka sab screenshots jaayega tere parents ka number pe chuttiye, conference pe lelenge tera baap ko<br/>
-            </p>
-    </main> // writing this on 17/7/24, Sorry Sana, my futue wife, i have no other option left for you to complete your dream, I trust and love you, i know other boys ultimate motive would be to touch you in bad places and doing dirty things, take dirty benefits in the name of love but i my love is not the same i want to see you happy and safe around me all the time, tbh i sometimes feel i can be with you whole life without even touching you but still you beside me :)  may god always protect you from these scavengers
+          <span className='text-5xl'>though I woulda deny my most loyal and trustworthy friends challenge?</span>
+              <br/>
+              1 year, 132 lbs, muscular+lean, accepted, NO G friends<br/>
+              my challenge: 1 year, 88lbs, lean+10%bodyFat, no guy friends<br/>
+              <span className='text-5xl'>{"Don't worry I'm never making the mistake of choosing another girl over you ever again, I KNOW YOU'RE FAR MORE LOYAL THAN ME, INTROVERTED OWL. Flames told me the number of days you cried for me never again :("}</span>
+               </p>
+    </main> 
   );
 }
