e18d20444f65e52c9ddd611b675e35def0ffe174
pixelDriftadv <pixeldrift.adv@proton.me>
2024-07-25 01:18:40 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
             <p className="z-10  whitespace-pre-wrap text-center text-xl font-bold tracking-tighter ">
           <span className='text-5xl'>though I woulda deny my most loyal and trustworthy friends challenge?</span>
               <br/>
-              1 year, 132 lbs, muscular+lean, accepted, NO Girl friends (never again ngl fr)<br/>
+              1 year, 132 lbs, muscular+lean, accepted, NO Girl friends (never again ngl fr/except naomi, andy, emy, linda and havana)<br/>
               my challenge: 1 year, about 88lbs, lean+10%bodyFat, no Guy friends (except our teammates)<br/>
               <span className='text-5xl'>{"Forgive me & Don't worry I'm never ever making the mistake of choosing another girl over you, really ever again, I KNOW YOU'RE FAR MORE LOYAL THAN ME, INTROVERTED OWL. Basically everyday Flames reminded me the number of days you cried for me, don't ever again :("}</span>
               <br/>
